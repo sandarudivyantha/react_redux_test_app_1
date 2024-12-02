489046aab3ead179e2b2cd85cc4fbb3839d0cd97
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-12-02 20:23:01 +0530

    Post List created. Not posts are showing in the browser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import ButtonComponents from "./components/ButtonComponents";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <div className="App">
       <h1>wds</h1>
       <ButtonComponents />
+      <br />
+      <PostList />
     </div>
   );
 }
