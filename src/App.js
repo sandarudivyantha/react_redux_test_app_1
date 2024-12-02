2535b060a2f54e5f17cec1385b474f9cc2cb4601
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-12-02 15:48:39 +0530

    reducers created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import ButtonComponents from './components/ButtonComponents';
+import "./App.css";
+import ButtonComponents from "./components/ButtonComponents";
 
 function App() {
   return (
     <div className="App">
       <h1>wds</h1>
-      <ButtonComponents/>
+      <ButtonComponents />
     </div>
   );
 }
